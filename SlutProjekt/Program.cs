acc17626665fad69c5470db39fed7f1c60475131
Calle1122 <carl@lindstedt.net>
2020-03-23 09:40:35 +0100

    Har nu skapat en tvådimensionell array och börjat testa hur det ska fungera.

--- SlutProjekt/Program.cs
+++ SlutProjekt/Program.cs
@@ -10,6 +10,20 @@ namespace SlutProjekt
     {
         static void Main(string[] args)
         {
+            int[,] board = new int[15, 15];
+
+            for (int y = 0; y < board.GetLength(1); y++)
+            {
+                for (int x = 0; x < board.GetLength(0); x++)
+                {
+                    board[x, y] = 4;
+
+                    if (x == 0)
+                    {
+                        board[x, y] = 2;
+                    }
+                }
+            }
         }
     }
 }
