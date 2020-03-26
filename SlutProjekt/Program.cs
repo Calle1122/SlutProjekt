d2b9a2dfe1ae7ee1f836011cddfd5841097d8b9a
Calle1122 <carl@lindstedt.net>
2020-03-26 10:06:10 +0100

    Ritat ut mitt "Tre i rad"-spelbräde

--- SlutProjekt/Program.cs
+++ SlutProjekt/Program.cs
@@ -10,31 +10,23 @@ namespace SlutProjekt
     {
         static void Main(string[] args)
         {
-            int[,] board = new int[15, 15];
+            int[,] board = new int[3, 3];
 
-            for (int y = 0; y < board.GetLength(1); y++)
-            {
-                for (int x = 0; x < board.GetLength(0); x++)
-                {
-                    board[x, y] = 4;
+            Console.WriteLine("   Tic-Tac-Toe");
+            Console.WriteLine();
+            Console.WriteLine("     │     │");
+            Console.WriteLine("     │     │");
+            Console.WriteLine("     │     │");
+            Console.WriteLine("─────┼─────┼─────");
+            Console.WriteLine("     │     │");
+            Console.WriteLine("     │     │");
+            Console.WriteLine("     │     │");
+            Console.WriteLine("─────┼─────┼─────");
+            Console.WriteLine("     │     │");
+            Console.WriteLine("     │     │");
+            Console.WriteLine("     │     │");
+            Console.Read();
 
-                    if (x == 0)
-                    {
-                        board[x, y] = 2;
-                    }
-                }
-            }
-
-            for (int y = 0; y < board.GetLength(1); y++)
-            {
-                for (int x = 0; x < board.GetLength(0); x++)
-                {
-                    Console.Write(board[x, y] + " ");
-                }
-                Console.WriteLine();
-            }
-
-            Console.ReadLine();
         }
     }
 }
