52970c3b0d4987d11a4db8b3a27555657038f62b
Calle1122 <carl@lindstedt.net>
2020-03-30 09:03:45 +0200

    spelbrädet ritar nu ut board[x, y]

--- SlutProjekt/Program.cs
+++ SlutProjekt/Program.cs
@@ -13,24 +13,38 @@ namespace SlutProjekt
             int[,] board = new int[3, 3];
             int[,] cursor = new int[3, 3];
 
-            Console.WriteLine("   Tic-Tac-Toe");
+            for (int y = 0; y < board.GetLength(1); y++)
+            {
+                for (int x = 0; x < board.GetLength(0); x++)
+                {
+                    board[x, y] = 0;
+                }
+            }
+
+            Console.WriteLine("      Tic-Tac-Toe");
             Console.WriteLine();
-            Console.WriteLine("     │     │");
-            Console.WriteLine("     │     │");
-            Console.WriteLine("     │     │");
-            Console.WriteLine("─────┼─────┼─────");
-            Console.WriteLine("     │     │");
-            Console.WriteLine("     │     │");
-            Console.WriteLine("     │     │");
-            Console.WriteLine("─────┼─────┼─────");
-            Console.WriteLine("     │     │");
-            Console.WriteLine("     │     │");
-            Console.WriteLine("     │     │");
+            Console.WriteLine("     1     2     3");
+            Console.WriteLine("        │     │");
+            Console.WriteLine(" 1   " + board[0, 0] + "  │  " + board[0, 1] + "  │  " + board[0, 2]);
+            Console.WriteLine("        │     │");
+            Console.WriteLine("   ─────┼─────┼─────");
+            Console.WriteLine("        │     │");
+            Console.WriteLine(" 2   " + board[1, 0] + "  │  " + board[1, 1] + "  │  " + board[1, 2]);
+            Console.WriteLine("        │     │");
+            Console.WriteLine("   ─────┼─────┼─────");
+            Console.WriteLine("        │     │");
+            Console.WriteLine(" 3   " + board[2, 0] + "  │  " + board[2, 1] + "  │  " + board[2, 2]);
+            Console.WriteLine("        │     │");
             Console.Read();
 
         }
 
         static void moveCursor()
+        {
+
+        }
+
+        static void drawXO()
         {
 
         }
